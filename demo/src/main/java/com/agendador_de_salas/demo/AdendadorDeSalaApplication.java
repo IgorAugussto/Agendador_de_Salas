c669a73ad4f4ff932ec950b407beb0d5121969c3
Igor Augusto <igor-augustofc@hotmail.com>
2025-07-15 12:16:10 -0300

    First commit

---
+++ demo/src/main/java/com/agendador_de_salas/demo/AdendadorDeSalaApplication.java
@@ -0,0 +1,13 @@
+package com.agendador_de_salas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdendadorDeSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdendadorDeSalaApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/agendador_de_salas/demo/AdendadorDeSalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendador_de_salas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdendadorDeSalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
